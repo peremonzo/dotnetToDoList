2c7829949cf5e364abe02c829c4b1a96ebd03a4e
unknown <peremonzohervas@gmail.com>
2022-10-17 16:57:17 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplicationC.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
